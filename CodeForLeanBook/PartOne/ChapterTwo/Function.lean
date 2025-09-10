e7b97305cbafd2d7cbd66da565521fa1554eabdf
jjaassoonn <jujian.zhang1998@outlook.com>
2025-09-10 22:54:31 +0100

    a bit more

---
+++ CodeForLeanBook/PartOne/ChapterTwo/Function.lean
@@ -0,0 +1,21 @@
+import Mathlib
+
+def PreZHat : Type := Π (n : ℕ), ZMod n
+example : Type := ∀ (n : ℕ), ZMod n
+example : Type := (n : ℕ) → ZMod n
+
+def FunctionType1 (α β : Type) : Type := α → β
+def FunctionType2 (α β : Type) : Type := Π (a : α), β
+
+example : FunctionType1 = FunctionType2 := rfl
+
+example : (fun x : ZMod 4 ↦ (2 * x + 1) ^ 2) = (fun x : ZMod 4 ↦ 1) := by
+  ext x
+  ring_nf
+  simp [show (4 : ZMod 4) = 0 by rfl]
+
+example (X Y Z : Type) (f : X → Y) (g : Y → Z) (H : Function.Injective <| g ∘ f) :
+    Function.Injective f := by
+  intros x x' h
+  apply_fun g at h
+  exact H h
---
+++ CodeForLeanBook/PartOne/ChapterTwo/Inductive.lean
@@ -0,0 +1,38 @@
+inductive F2 : Type
+| zero : F2
+| one : F2
+
+#check F2.rec
+
+/-
+Lean 中自然数已经被定义了
+inductive Nat : Type
+| zero : Nat
+| succ (n : Nat): Nat
+-/
+
+#check Nat.rec
+
+
+def Nat.factorial : Nat → Nat :=
+Nat.rec 1 (fun n factorial_n ↦ (n + 1) * factorial_n)
+
+#eval Nat.factorial 10
+
+def Nat.factorial'
+| 0 => 1
+| succ n => (n + 1) * (factorial' n)
+
+#eval Nat.factorial' 10
+
+inductive Integer : Type
+| nonneg : Nat → Integer
+| neg : Nat → Integer
+
+
+-- def Or.swap (P Q : Prop) : Or P Q → Or Q P := Or.rec _ _
+
+#check Exists
+
+inductive Exists' {α : Type} (p : α → Prop) : Prop
+| intro (w : α) (h : p w) : Exists' p
